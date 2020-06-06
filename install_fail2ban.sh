6c4f5a85857082721f2c8851ebc8492d083a3161
Martin Pecka <peci1@seznam.cz>
2020-06-06 23:55:37 +0200

    Install 2to3 if not present

--- install_fail2ban.sh
+++ install_fail2ban.sh
@@ -12,6 +12,11 @@ opkg install git
 opkg install git-http
 opkg install python3-lib2to3
 
+if [ ! -f /usr/bin/2to3 ]; then
+  cp usr/bin/2to3 /usr/bin/
+  chmod +x /usr/bin/2to3
+fi
+
 mkdir -p "${download_dir}"
 cd "${download_dir}"
 git clone https://github.com/fail2ban/fail2ban.git
