c7a95cd1a0243fd7f0c73f5b324ac83c6493ef73
Martin Pecka <peci1@sezam.cz>
2021-03-07 00:43:52 +0100

    Add firewall restart hook

--- install_f2b_config.sh
+++ install_f2b_config.sh
@@ -13,3 +13,13 @@ cp "${SCRIPT_DIR}/etc/config/fail2ban" /etc/config/fail2ban
 cp "${SCRIPT_DIR}/etc/fail2ban/action.d/"* /etc/fail2ban/action.d/
 cp "${SCRIPT_DIR}/etc/fail2ban/fail2ban.d/uci.conf" /etc/fail2ban/fail2ban.d/
 cp "${SCRIPT_DIR}/etc/init.d/fail2ban" /etc/init.d/fail2ban
+cp "${SCRIPT_DIR}/etc/firewall.fail2ban" /etc/firewall.fail2ban
+
+# unfortunately, UCI doesn't provide a nice way to add an anonymous section only if it doesn't already exist
+if ! uci show firewall | grep -q firewall.fail2ban; then
+  name="$(uci add firewall include)"
+  uci set "firewall.${name}.path=/etc/firewall.fail2ban"
+  uci set "firewall.${name}.enabled=1"
+  echo -e "Adding the following UCI config:\n $(uci changes)"
+  uci commit
+fi
